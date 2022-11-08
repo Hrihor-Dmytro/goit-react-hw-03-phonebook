71ef14a873653239cfc59181ab30e61976517457
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2022-11-09 01:50:58 +0200

    fix CDU CDM

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
   handleSubmit = values => {
     const { name, number } = values;
 
